267aff6375a473668106c981e4064708b5018466
KleytonV2 <kleyton@hotmail.es>
2025-04-19 20:35:21 +0200

    fix: home bg image was moved from global.css to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Navbar from '@/app/components/Navbar';
 import HeroSection from '@/app/components/HeroSection';
 import PageContent from '@/app/components/PageContent';
 import FooterSection from '@/app/components/FooterSection';
+import bannerOverlay from "@/app/assets/banner_overlay.png";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <section id='home' data-bg="dark" className="bg-fixed w-full h-screen top-0 left-0 bg-cover bg-center">
+      <section id='home' data-bg="dark" className="bg-fixed w-full h-screen top-0 left-0 bg-cover bg-center"
+        style={{ backgroundImage: `url(${bannerOverlay.src})` }}>
         <HeroSection />
       </section>
       <PageContent />
