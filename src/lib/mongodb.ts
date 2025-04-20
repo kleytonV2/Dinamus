fa48d3b912b937beedf8ead9509abab08bb16e6e
KleytonV2 <kleyton@hotmail.es>
2025-04-20 22:09:47 +0200

    fix: leaving modb connection how it was before

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,34 +1,29 @@
 import mongoose from "mongoose";
-
-const MONGODB_URI = process.env.MONGODB_URI as string;
-
-if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+ 
+ const MONGODB_URI = process.env.MONGODB_URI as string;
+ 
+ if (!MONGODB_URI) {
+   throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+ }
+ 
+ let cachedConnection: typeof mongoose | null = null;
+ 
+ export const connectToDatabase = async () => {
+     if (cachedConnection) {
+         console.log("Using cached MongoDB connection");
+         return cachedConnection;
+     }
+ 
+     try {
+         console.log("Connecting to MongoDB...");
+         cachedConnection = await mongoose.connect(MONGODB_URI, {
+             dbName: "Dinamus", 
+         });
+ 
+         console.log("MongoDB Connected ✅");
+         return cachedConnection;
+     } catch (error) {
+         console.error("MongoDB Connection Error ❌", error);
+         throw new Error("Failed to connect to MongoDB");
+     }
 }
-
-let cached = (global as any).mongoose || { conn: null, promise: null };
-
-export const connectToDatabase = async () => {
-    try {
-
-        if (cached.conn) return cached.conn;
-
-        if (!cached.promise) {
-            cached.promise = mongoose.connect(MONGODB_URI, {
-                dbName: "Dinamus", 
-                bufferCommands: false
-            });
-        }
-    
-        console.log("Connecting to MongoDB...");
-        
-        cached.conn = await cached.promise;
-        (global as any).mongoose = cached;
-
-        console.log("MongoDB Connected!");
-        return cached.conn;
-    } catch (error) {
-        console.error("MongoDB Connection Error", error);
-        throw new Error("Failed to connect to MongoDB");
-    }
-};
